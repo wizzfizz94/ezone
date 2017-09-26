0b330c0066851bbd7dac7a64e3bb9b3bcb7552cc
xanderle <alexander.emery@bigpond.com>
2017-09-26 11:05:56 +0800

    Single door detection

---
+++ vision/orb.py
@@ -0,0 +1,43 @@
+import numpy as np
+import cv2
+from matplotlib import pyplot as plt
+MIN_MATCH_COUNT = 9
+img1 = cv2.imread('door.png',cv2.IMREAD_GRAYSCALE)          # queryImage
+img2 = cv2.imread('door1.png',cv2.IMREAD_GRAYSCALE) # trainImage
+
+# Initiate SIFT detector
+orb = cv2.ORB_create()
+
+# find the keypoints and descriptors with SIFT
+kp1, des1 = orb.detectAndCompute(img1,None)
+kp2, des2 = orb.detectAndCompute(img2,None)
+# create BFMatcher object
+bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
+
+# Match descriptors.
+matches = bf.match(des1,des2)
+# Sort them in the order of their distance.
+matches = sorted(matches, key = lambda x:x.distance)
+
+good = matches[:10]
+
+if len(good)>MIN_MATCH_COUNT:
+    src_pts = np.float32([ kp1[m.queryIdx].pt for m in good ]).reshape(-1,1,2)
+    dst_pts = np.float32([ kp2[m.trainIdx].pt for m in good ]).reshape(-1,1,2)
+    M, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC,5.0)
+    matchesMask = mask.ravel().tolist()
+    h,w = img1.shape
+    pts = np.float32([ [0,0],[0,h-1],[w-1,h-1],[w-1,0] ]).reshape(-1,1,2)
+    dst = cv2.perspectiveTransform(pts,M)
+    img2 = cv2.polylines(img2,[np.int32(dst)],True,155,3, cv2.LINE_AA)
+else:
+    print( "Not enough matches are found - {}/{}".format(len(good), MIN_MATCH_COUNT) )
+    matchesMask = None
+
+draw_params = dict(matchColor = (0,255,0), # draw matches in green color
+                   singlePointColor = None,
+                   matchesMask = matchesMask, # draw only inliers
+                   flags = 2)
+img3 = cv2.drawMatches(img1,kp1,img2,kp2,good,None,**draw_params)
+print(matchesMask)
+plt.imshow(img3, 'gray'),plt.show()
